a0b0deedea173f72080981f0511934ff9feacb74
p0tatoes <76986919+p0tatoes@users.noreply.github.com>
2023-11-23 22:06:33 +0800

    feat: back press on products list view (main activity) now closes the view

--- app/src/main/java/com/example/ite2exam/MainActivity.kt
+++ app/src/main/java/com/example/ite2exam/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.Firebase
@@ -24,23 +25,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var _signoutButton: Button
     private lateinit var _productsListLayout: LinearLayout
 
+    // back press event callback; just finishes the activity
+    private val backPressCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Firestore Instance
         db = Firebase.firestore
 
+        // Firebase Authentication Instance
         auth = Firebase.auth
         val currentUser: FirebaseUser = auth.currentUser!!
 
+        // UI Elements
         _productsListLayout = findViewById(R.id.productListLayout)
         _welcomeTextView = findViewById(R.id.welcomeTextView)
         _signoutButton = findViewById(R.id.signoutButton)
 
+        // Retrieves registered display name
         val name = currentUser.displayName
-
         _welcomeTextView.text = "Welcome, ${name}"
 
+        // Handles back press events
+        onBackPressedDispatcher.addCallback(this, backPressCallback)
+
+        // Signout button function
         _signoutButton.setOnClickListener {
             auth.signOut()
             finish()
@@ -55,16 +70,19 @@ class MainActivity : AppCompatActivity() {
                 val prodImageURL = arrProdImages[0]
                 val prodDescription = product.data["description"].toString()
 
+                // Populates res/product_card.xml interface with product information
                 val productItemView = layoutInflater.inflate(R.layout.product_card, null)
 
                 val prodImageView: ImageView = productItemView.findViewById(R.id.prodImageView)
                 val prodNameView: TextView = productItemView.findViewById(R.id.prodNameTextView)
-                val prodPriceView: TextView = productItemView.findViewById(R.id.prodPriceTextView)
+                val prodPriceView: TextView =
+                    productItemView.findViewById(R.id.prodPriceTextView)
 
                 prodNameView.text = prodName
                 prodPriceView.text = "₱${prodPrice}"
                 Glide.with(this).load(prodImageURL).into(prodImageView)
 
+                // Adds an event listener for click a product in the list. Opens a new activity containing all product details of the clicked product
                 productItemView.setOnClickListener {
                     val toProductListing: Intent = Intent(this, ProductListing::class.java)
                     toProductListing.putExtra("prodName", prodName)
@@ -78,5 +96,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
 }
